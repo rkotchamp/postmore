8cb95332d5ae58386a0afcbab6f1fd6f56a8afd3
Reuben <treuben90@yahoo.com>
2025-07-13 00:36:06 +0300

    deploy 33

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { LoginForm } from "./components/LoginForm";
 import { LoginLayout } from "./components/LoginLayout";
 
 export default function Login() {
   return (
     <LoginLayout>
-      <LoginForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </LoginLayout>
   );
 }
--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { RegisterForm } from "./components/RegisterForm";
 import { RegisterLayout } from "./components/RegisterLayout";
 
 export default function Register() {
   return (
     <RegisterLayout>
-      <RegisterForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <RegisterForm />
+      </Suspense>
     </RegisterLayout>
   );
 }
