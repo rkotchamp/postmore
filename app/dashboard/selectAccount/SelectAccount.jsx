ac0ab5cdd15ba77a2d040711792eb13f3753b464
Reuben <treuben90@yahoo.com>
2025-07-23 20:33:43 +0300

    deploy 90

--- app/dashboard/selectAccount/SelectAccount.jsx
+++ app/dashboard/selectAccount/SelectAccount.jsx
@@ -9,7 +9,7 @@ import {
 } from "@/app/components/ui/accordion";
 import { Username } from "@/app/authenticate/components/Username";
 import { Button } from "@/app/components/ui/button";
-import { Instagram, Twitter, Facebook, AtSign, Youtube, Linkedin } from "lucide-react";
+import { Instagram, Twitter, Facebook, AtSign, Youtube } from "lucide-react";
 import { useFetchAllAccountsContext } from "@/app/context/FetchAllAccountsContext";
 import { useRouter } from "next/navigation";
 import { usePostStore } from "@/app/lib/store/postStore";
@@ -42,6 +42,16 @@ const BlueskyIcon = ({ className }) => (
     <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2Z" />
     <path d="M17 12c0 2.8-2.2 5-5 5s-5-2.2-5-5 2.2-5 5-5 5 2.2 5 5Z" />
     <path d="M12 7v0" />
+  </svg>
+);
+const LinkedInIcon = ({ className }) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
   </svg>
 );
 // --- End Icon Components ---
@@ -200,7 +210,7 @@ export function SelectAccount() {
       case "bluesky":
         return <BlueskyIcon {...iconProps} />;
       case "linkedin":
-        return <Linkedin {...iconProps} />;
+        return <LinkedInIcon {...iconProps} />;
       default:
         return null;
     }
