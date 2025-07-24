995e8d1411a0022de5627d35a3037e3e453e27f0
Reuben <treuben90@yahoo.com>
2025-07-24 23:53:05 +0300

    deploy 93

--- app/auth/register/components/RegisterLayout.jsx
+++ app/auth/register/components/RegisterLayout.jsx
@@ -56,23 +56,6 @@ export function RegisterLayout({ children }) {
             </p>
           </div>
           {children}
-          <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking create account, you agree to our{" "}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Terms of Service
-            </Link>{" "}
-            and{" "}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Privacy Policy
-            </Link>
-            .
-          </p>
         </div>
       </div>
     </div>
